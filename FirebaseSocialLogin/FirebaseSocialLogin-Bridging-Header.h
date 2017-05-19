3139ba13a7c41b098e249abd22e625653f5cfc11
Farrukh Askari <farrukh.askari01@gmail.com>
2017-05-19 22:18:19 +0500

    removed payment pods and pod update

---
+++ FirebaseSocialLogin/FirebaseSocialLogin-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  FirebaseSocialLogin-Bridging-Header.h
+//  FirebaseSocialLogin
+//
+//  Created by Syed Askari on 18/05/2017.
+//  Copyright © 2017 Lets Build That App. All rights reserved.
+//
+
+#ifndef FirebaseSocialLogin_Bridging_Header_h
+#define FirebaseSocialLogin_Bridging_Header_h
+
+
+#endif /* FirebaseSocialLogin_Bridging_Header_h */
